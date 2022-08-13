eddad65a9a712a709be8dce14c9e7a8c3ec8b779
rainerramos <rainermr89@hotmail.com>
2022-08-13 16:42:52 -0300

    Implementando DTOs (ViewModels)

--- src/DevIO.Api/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevIO.Api.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
